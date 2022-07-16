d3884e3cac4b797a0d457003226aed8843b370e2
Сергей Боровиков <BorovikovSergei@MacBook-Air-Maria.local>
2022-07-16 14:40:08 +0300

    add task3

---
+++ Task_3.py
@@ -0,0 +1,31 @@
+# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+# Пример:
+
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+
+def float_part(float_number):
+    number = float_number % 1
+    return number
+    
+
+def clear_list(some_list):
+    new_list = []
+    for i in range(len(my_list)):
+        new_list.append(round(float_part(my_list[i]), 2))
+        
+    for i in range (len(new_list)):
+        if new_list[i] == 0:
+            new_list[i], new_list[len(new_list) - 1] = new_list[len(new_list) - 1], new_list[i]
+            
+    new_list.pop() 
+    return new_list
+
+
+   
+my_list = [1.1, 1.2, 3.1, 5, 10.01]         
+result = max(clear_list(my_list)) - min(clear_list(my_list))
+print(result)
+
+    
